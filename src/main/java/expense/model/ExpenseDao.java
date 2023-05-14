fb35deb08d53571baa423660732729561f73f685
thecalver96 <sisamartin96@gmail.com>
2023-05-14 16:05:16 +0200

    Refactored ExpenseDao and ExpenseDaoImpl

--- src/main/java/expense/model/ExpenseDao.java
+++ src/main/java/expense/model/ExpenseDao.java
@@ -8,61 +8,17 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.DoubleStream;
 
-public class ExpenseDao extends GenericJpaDao<Expense> implements ExpenseDaoInt{
+public interface ExpenseDao {
     @Transactional
-    public List<Expense> findBetweenDates(LocalDate startDate, LocalDate endDate){
-        TypedQuery<Expense> typedQuery = (TypedQuery<Expense>) entityManager.createQuery(
-                "SELECT e FROM " + entityClass.getSimpleName()
-                        + " e WHERE e.date >=: startDate and e.date <=: endDate", entityClass);
-        typedQuery.setParameter("startDate", startDate);
-        typedQuery.setParameter("endDate", endDate);
+    public List<Expense> findBetweenDates(LocalDate startDate, LocalDate endDate);
 
-        return typedQuery.getResultList();
-    }
+    public Double getBalance();
 
-    public Double getBalance() {
-
-     return findAll()
-                        .stream()
-                        .filter(expense -> expense.getType().equals(Expense.Type.INCOME))
-                        .flatMapToDouble(expense -> DoubleStream.of(expense.getCost()))
-                        .sum()
-                        -
-                        findAll().stream().filter(expense -> expense.getType().equals(Expense.Type.EXPENSE))
-                        .flatMapToDouble(expense -> DoubleStream.of(expense.getCost()))
-                        .sum();
-    }
-
-    public Double getSumOfExpenses(){
-        return findBetweenDates(LocalDate.now().minusDays(30), LocalDate.now())
-                .stream()
-                .filter(expense -> expense.getType().equals(Expense.Type.EXPENSE))
-                .mapToDouble(Expense::getCost)
-                .sum();
-    }
+    public Double getSumOfExpenses();
 
 
-    public Double getExpenseByCategory(Expense.MainCategory category){
-        return findAll().stream()
-                .filter(expense -> expense.getType().equals(Expense.Type.EXPENSE)
-                &&
-                        expense.getCategory().equals(category))
-                .mapToDouble(Expense::getCost)
-                .sum()
-                ;
-
-    }
+    public Double getExpenseByCategory(Expense.MainCategory category);
     @Transactional
-    public List<Expense> getSearch(String searchTerm, LocalDate startDate, LocalDate endDate, List<Expense.MainCategory> categories){
-        TypedQuery<Expense> typedQuery = (TypedQuery<Expense>) entityManager.createQuery(
-                "SELECT e FROM " + entityClass.getSimpleName()
-                        + " e WHERE e.title like '%" + searchTerm +"%' and ( e.date >=: startDate and e.date <=: endDate) and e.category IN :categories", entityClass);
-        typedQuery.setParameter("startDate", startDate);
-        typedQuery.setParameter("endDate", endDate);
-        typedQuery.setParameter("categories", categories);
-
-
-        return typedQuery.getResultList();
-    }
+    public List<Expense> getSearch(String searchTerm, LocalDate startDate, LocalDate endDate, List<Expense.MainCategory> categories);
 
 }
---
+++ src/main/java/expense/model/ExpenseDaoImpl.java
@@ -0,0 +1,70 @@
+package expense.model;
+
+import jpa.GenericJpaDao;
+
+import javax.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+public class ExpenseDaoImpl extends GenericJpaDao<Expense> implements ExpenseDao{
+
+    @Override
+    public List<Expense> findBetweenDates(LocalDate startDate, LocalDate endDate) {
+        TypedQuery<Expense> typedQuery = (TypedQuery<Expense>) entityManager.createQuery(
+                "SELECT e FROM " + entityClass.getSimpleName()
+                        + " e WHERE e.date >=: startDate and e.date <=: endDate", entityClass);
+        typedQuery.setParameter("startDate", startDate);
+        typedQuery.setParameter("endDate", endDate);
+
+        return typedQuery.getResultList();
+    }
+
+    @Override
+    public Double getBalance() {
+
+        return findAll()
+                .stream()
+                .filter(expense -> expense.getType().equals(Expense.Type.INCOME))
+                .flatMapToDouble(expense -> DoubleStream.of(expense.getCost()))
+                .sum()
+                -
+                findAll().stream().filter(expense -> expense.getType().equals(Expense.Type.EXPENSE))
+                        .flatMapToDouble(expense -> DoubleStream.of(expense.getCost()))
+                        .sum();
+    }
+
+    @Override
+    public Double getSumOfExpenses() {
+        return findBetweenDates(LocalDate.now().minusDays(30), LocalDate.now())
+                .stream()
+                .filter(expense -> expense.getType().equals(Expense.Type.EXPENSE))
+                .mapToDouble(Expense::getCost)
+                .sum();
+    }
+
+    @Override
+    public Double getExpenseByCategory(Expense.MainCategory category) {
+        return findAll().stream()
+                .filter(expense -> expense.getType().equals(Expense.Type.EXPENSE)
+                        &&
+                        expense.getCategory().equals(category))
+                .mapToDouble(Expense::getCost)
+                .sum()
+                ;
+
+    }
+
+    @Override
+    public List<Expense> getSearch(String searchTerm, LocalDate startDate, LocalDate endDate, List<Expense.MainCategory> categories) {
+        TypedQuery<Expense> typedQuery = (TypedQuery<Expense>) entityManager.createQuery(
+                "SELECT e FROM " + entityClass.getSimpleName()
+                        + " e WHERE e.title like '%" + searchTerm +"%' and ( e.date >=: startDate and e.date <=: endDate) and e.category IN :categories", entityClass);
+        typedQuery.setParameter("startDate", startDate);
+        typedQuery.setParameter("endDate", endDate);
+        typedQuery.setParameter("categories", categories);
+
+
+        return typedQuery.getResultList();
+    }
+}
